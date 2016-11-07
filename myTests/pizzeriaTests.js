d4381cfa1007678d88e1efc8f2bb9fe2821f6949
Programmist <717720@list.ru>
2016-11-07 18:30:17 +0900

    tests

--- myTests/pizzeriaTests.js
+++ myTests/pizzeriaTests.js
@@ -8,15 +8,28 @@ suite("pizzeria tests", function() {
 
     suite("customer pay $10 and ask thin chicken pizza without olives", function() {
 
-        test("customer get chicken pizza", function() {
-            let money = 10;
-            let pizza = new NewPizzaRequest()
+        let money, pizza;
+
+        setup(function() {
+            money = 10;
+            pizza = new NewPizzaRequest()
                 .pay(money)
                 .onDough("thin")
                 .withFilling("chicken")
                 .withoutOlives()
                 .build();
+        });
+
+        test("customer get chicken pizza", function() {
             assert.equal(pizza.filling, "chicken");
+        });
+
+        test("customer get thin pizza", function() {
+            assert.equal(pizza.dough, "thin");
+        });
+
+        test("customer get thin pizza", function() {
+            assert.equal(pizza.olives, false);
         });
     });
 
