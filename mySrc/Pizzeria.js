2a2bfecd984783a547e2c9491bea4ad5413956a0
admin <717720@list.ru>
2016-12-04 16:15:08 +0900

    fix

--- mySrc/Pizzeria.js
+++ mySrc/Pizzeria.js
@@ -1,30 +1,18 @@
-
 const pizzaPrice = 10;
 const mushroomCoefficient = 0.9;
 
-export function PizzaRequest(money, dough, filling, olives, count) {
-    if (!count) count = 1;
+export function ClientRequest(money, dough, filling, olives, count) {
 
-    let price = pizzaPrice;
-
-    if (filling == "mushrooms") {
-        price = price * mushroomCoefficient;
-    }
-
-    if (money >= price * count) {
-        this.dough = dough;
-        this.filling = filling;
-        this.olives = olives;
-        this.count = count;
-        this.back = money - (price * count);
-    } else {
-        this.count = 0;
-    }
+    this.money = money;
+    this.dough = dough;
+    this.filling = filling;
+    this.olives = olives;
+    this.count = count;
 
     return this;
 }
 
-export function NewPizzaRequest() {
+export function NewClientRequest() {
 
     let data = {};
 
@@ -33,29 +21,66 @@ export function NewPizzaRequest() {
         return this;
     };
 
-    this.onDough = function(dough) {
+    this.onDough = function (dough) {
         data.dough = dough;
         return this;
     };
 
-    this.withFilling = function(filling) {
+    this.withFilling = function (filling) {
         data.filling = filling;
         return this;
     };
 
-    this.withoutOlives = function() {
+    this.withoutOlives = function () {
         data.olives = false;
         return this;
     };
 
     this.count = function (count) {
-      data.count = count;
-      return this;
+        data.count = count;
+        return this;
     };
 
-    this.build = function() {
-        return new PizzaRequest(data.money, data.dough, data.filling, data.olives, data.count);
+    this.build = function () {
+        return new ClientRequest(data.money, data.dough, data.filling, data.olives, data.count);
     };
 
+
+}
+
+export function Pizzeria() {
+
+    this.pizza = null;
+
+    this.pushOrder = function (request) {
+
+        if (!request.count) request.count = 1;
+
+        let price = pizzaPrice;
+
+        if (request.filling == "mushrooms") {
+            price = price * mushroomCoefficient;
+        }
+
+        let order = {};
+
+        if (request.money >= price * request.count) {
+            order.dough = request.dough;
+            order.filling = request.filling;
+            order.olives = request.olives;
+            order.count = request.count;
+            order.back = request.money - (price * request.count);
+        } else {
+            order.count = 0;
+        }
+
+        this.pizza = order;
+    };
+
+    this.getPizza = function () {
+        return this.pizza;
+    };
+
+    return this;
 
 }
--- myTests/pizzeriaTests.js
+++ myTests/pizzeriaTests.js
@@ -1,100 +1,153 @@
 import assert from 'assert';
-import  {NewPizzaRequest} from "../mySrc/Pizzeria";
+import  {NewClientRequest, Pizzeria} from "../mySrc/Pizzeria";
 
-suite("pizzeria tests", function() {
+suite("pizzeria tests", function () {
 
-    setup(function() {
+    setup(function () {
     });
 
-    suite("customer pay $10 and ask thin chicken pizza without olives", function() {
+    suite("customer pay $10 and ask thin chicken pizza without olives", function () {
 
-        let money, pizza;
+        let pizzeria;
 
-        setup(function() {
-            money = 10;
-            pizza = new NewPizzaRequest()
+        setup(function () {
+            pizzeria = new Pizzeria();
+        });
+
+        test("customer get chicken pizza", function () {
+            let money = 10;
+            let request = new NewClientRequest()
                 .pay(money)
                 .onDough("thin")
                 .withFilling("chicken")
                 .withoutOlives()
                 .build();
-        });
+            pizzeria.pushOrder(request);
+            let pizza = pizzeria.getPizza();
 
-        test("customer get chicken pizza", function() {
             assert.equal(pizza.filling, "chicken");
         });
 
-        test("customer get thin pizza", function() {
+        test("customer get thin pizza", function () {
+            let money = 10;
+            let request = new NewClientRequest()
+                .pay(money)
+                .onDough("thin")
+                .withFilling("chicken")
+                .withoutOlives()
+                .build();
+            pizzeria.pushOrder(request);
+            let pizza = pizzeria.getPizza();
+
             assert.equal(pizza.dough, "thin");
         });
 
-        test("customer get thin pizza", function() {
+        test("customer get thin pizza", function () {
+            let money = 10;
+            let request = new NewClientRequest()
+                .pay(money)
+                .onDough("thin")
+                .withFilling("chicken")
+                .withoutOlives()
+                .build();
+            pizzeria.pushOrder(request);
+            let pizza = pizzeria.getPizza();
+
             assert.equal(pizza.olives, false);
         });
     });
 
-    suite("customer not pay and ask any pizza", function() {
+    suite("customer not pay and ask any pizza", function () {
 
-        test("customer do not get pizza", function() {
+        let pizzeria;
 
-            let pizza = new NewPizzaRequest()
+        setup(function () {
+            pizzeria = new Pizzeria();
+        });
+
+        test("customer do not get pizza", function () {
+            let request = new NewClientRequest()
                 .withFilling("chicken")
                 .build();
+            pizzeria.pushOrder(request);
+            let pizza = pizzeria.getPizza(request);
 
             assert.equal(pizza.count, 0);
         });
 
     });
 
-    suite("customer pay $30 ask 3 pizza", function() {
+    suite("customer pay $30 ask 3 pizza", function () {
 
-        test("customer get 3 pizza", function() {
+        let pizzeria;
+
+        setup(function () {
+            pizzeria = new Pizzeria();
+        });
+
+        test("customer get 3 pizza", function () {
             let money = 30;
             let count = 3;
-
-            let pizza = new NewPizzaRequest()
+            let request = new NewClientRequest()
                 .withoutOlives()
                 .count(count)
                 .pay(money)
                 .build();
+            pizzeria.pushOrder(request);
+            let pizza = pizzeria.getPizza();
 
             assert.equal(pizza.count, 3);
         });
 
     });
+    //
+    suite("customer pay more than required and get back", function () {
 
-    suite("customer pay more than required and get back", function() {
+        let pizzeria;
 
-        test("when customer pay $40 and ask 3 pizza then back equal $10", function() {
+        setup(function () {
+            pizzeria = new Pizzeria();
+        });
+
+        test("when customer pay $40 and ask 3 pizza then back equal $10", function () {
             let money = 40;
             let count = 3;
 
-            let pizza = new NewPizzaRequest()
+            let request = new NewClientRequest()
                 .count(count)
                 .pay(money)
                 .build();
+            pizzeria.pushOrder(request);
+            let pizza = pizzeria.getPizza();
 
             assert.equal(pizza.back, 40 - 3 * 10);
         });
 
     });
 
-    suite("customer ask pizza with mushrooms filling", function() {
+    suite("customer ask pizza with mushrooms filling", function () {
 
-        test("customer pay $9 and get pizza", function() {
+        let pizzeria;
+
+        setup(function () {
+            pizzeria = new Pizzeria();
+        });
+
+        test("customer pay $9 and get pizza", function () {
             let money = 9;
-
-            let pizza = new NewPizzaRequest()
+            let request = new NewClientRequest()
                 .pay(money)
                 .withFilling("mushrooms")
                 .build();
+            pizzeria.pushOrder(request);
+            let pizza = pizzeria.getPizza();
 
             assert.equal(pizza.count, 1);
         });
 
     });
 
-    teardown(function() {
+    teardown(function () {
     });
 
 });
